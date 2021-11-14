b470fd45b533a8753d320fbb3c0883175dd8ad48
rizkitriandani <rizkitriandani@yahoo.co.id>
2021-11-14 21:40:48 +0700

    Added App Theme.

---
+++ lib/shared/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = Color(0xff5C40CC);
+Color kBlackColor = Color(0xff1f1449);
+Color kWhiteColor = Color(0xfffff);
+Color kGreyColor = Color(0xff9698A9);
+Color kGreenColor = Color(0xff0EC3AE);
+Color kRedColor = Color(0xffEB70A5);
+Color kBackgroundColor = Color(0xffFAFAFA);
+Color kInactiveColor = Color(0xffDBD7EC);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBackgroundColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: kWhiteColor);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: kPrimaryColor,
+);
+TextStyle greenTextStyle = GoogleFonts.poppins(
+  color: kGreenColor,
+);
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
+
