de25d396629898a246307f38aa1a0229aac32be2
rizkitriandani <rizkitriandani@yahoo.co.id>
2021-11-15 15:47:01 +0700

    Added splash and get started page.

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Color kPrimaryColor = Color(0xff5C40CC);
-Color kBlackColor = Color(0xff1f1449);
-Color kWhiteColor = Color(0xfffff);
-Color kGreyColor = Color(0xff9698A9);
-Color kGreenColor = Color(0xff0EC3AE);
-Color kRedColor = Color(0xffEB70A5);
-Color kBackgroundColor = Color(0xffFAFAFA);
-Color kInactiveColor = Color(0xffDBD7EC);
+Color kPrimaryColor = const Color(0xff5C40CC);
+Color kBlackColor = const Color(0xff1f1449);
+Color kWhiteColor = const Color(0xffffffff);
+Color kGreyColor = const Color(0xff9698A9);
+Color kGreenColor = const Color(0xff0EC3AE);
+Color kRedColor = const Color(0xffEB70A5);
+Color kBackgroundColor = const Color(0xffFAFAFA);
+Color kInactiveColor = const Color(0xffDBD7EC);
 
 TextStyle blackTextStyle = GoogleFonts.poppins(
   color: kBackgroundColor,
@@ -36,4 +36,3 @@ FontWeight semiBold = FontWeight.w600;
 FontWeight bold = FontWeight.w700;
 FontWeight extraBold = FontWeight.w800;
 FontWeight black = FontWeight.w900;
-
